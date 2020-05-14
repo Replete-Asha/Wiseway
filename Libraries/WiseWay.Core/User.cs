c7d7a017482e8c45a639053be0cbaa50f972f971
asha <asha.shete@repleteinc.com>
2020-05-14 17:40:24 +0530

    Add all wiseway project files

---
+++ Libraries/WiseWay.Core/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WiseWay.Core
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter user name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+      
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
---
+++ Libraries/WiseWay.Extender/DBUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Felidae.Extender
+{
+    public class DBUtil
+    {
+        #region [Connection String]
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["DBConnection"].ToString();
+            }
+        }
+        #endregion
+    }
+}
